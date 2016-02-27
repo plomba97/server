0d261a8cfcdd708bc98c07a406f83505b886915c
plomba97 <plomba97@gmail.com>
2016-02-27 15:03:05 +0200

    commit

--- routes/inform.js
+++ routes/inform.js
@@ -48,8 +48,8 @@ router.post('/', function(req, res, next) {
 
 //Route: /inform/callsData Method:GET - Renders groups
 router.get('/callsData', function(req, res, next) {
-    var manager = callManager({ari:client, maxCalls: 10, appName: 'originate-test'});
-    response.send(manager);
+    var manager = callManager({ari:{}, maxCalls: 10, appName: 'originate-test'});
+    res.send(manager);
 });
 
 
