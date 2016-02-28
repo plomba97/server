a41834d614f3181a14906373c010b6dbd82a9844
plomba97 <plomba97@gmail.com>
2016-02-28 17:34:32 +0200

    sadsaca

--- public/javascripts/current-calls.js
+++ public/javascripts/current-calls.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
         //console.log(template({}));
         var intervalId = setInterval(function(){
             $.get('/data/calls', function(data){
-                if(data.calls[0]){
+                if(!data.calls[0]){
                     clearInterval(intervalId);
                 }
                 else{
