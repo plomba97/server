f7cd9ae51f31152dff1a6e0902a074d572380b81
plomba97 <plomba97@gmail.com>
2016-02-28 14:48:59 +0200

    changes

--- services/calls-manager.js
+++ services/calls-manager.js
@@ -21,87 +21,77 @@ var createManager = function (options) {
 };
 
 CallManager.prototype.addCall = function (callToAdd) {
-        if(!_.findWhere(this.calls, {'endPoint':callToAdd.endPoint})){
-            this.calls.push(callToAdd);
-            return true;
-        }
-        return false;
+    if(!_.findWhere(this.calls, {'endPoint':callToAdd.endPoint})){
+        this.calls.push(callToAdd);
+        return true;
+    }
+    return false;
 };
 
 CallManager.prototype.getActiveCalls = function () {
-        var currActiveCalls = _.filter(this.calls, function(element){return (element.state != 'None' && (element.state != 'FinishedAnswered' && element.state != 'FinishedUnAnswered'))});
-        return currActiveCalls.length;
+    var currActiveCalls = _.filter(this.calls, function(element){return (element.state != 'None' && (element.state != 'FinishedAnswered' && element.state != 'FinishedUnAnswered'))});
+    return currActiveCalls.length;
 };
 
 CallManager.prototype.startCalls = function () {
-        var obj = this;
-        var priority1 = _.filter(obj.calls, function(element){return (element.priority == 1)});
-        var priority2 = _.filter(obj.calls, function(element){return (element.priority == 2)});
-        var priority3 = _.filter(obj.calls, function(element){return (element.priority == 3)});
-        var priority4 = _.filter(obj.calls, function(element){return (element.priority == 4)});
-        var priority5 = _.filter(obj.calls, function(element){return (element.priority == 5)});
-    console.log(priority1);
-    console.log(priority2);
-    console.log(priority3);
-    console.log(priority4);
-    console.log(priority5);
-        var intervalId = setInterval(function(){
-            console.log(obj.getActiveCalls());
-            if(obj.getActiveCalls() < obj.maxCalls && priority1[0]){
-                priority1[0].startCall(obj.ari, obj.appName)
-                    .then(function(state){
-                        console.log(state);
-                    })
-                    .catch(function(err){
-
-                    });
-                priority1.shift();
-            }
-            else if(obj.getActiveCalls() < obj.maxCalls && priority2[0]){
-                priority2[0].startCall(obj.ari, obj.appName)
-                    .then(function(state){
-                        console.log(state);
-                    })
-                    .catch(function(err){
-
-                    });
-                priority2.shift();
-            }
-            else if(obj.getActiveCalls() < obj.maxCalls && priority3[0]){
-                priority3[0].startCall(obj.ari, obj.appName)
-                    .then(function(state){
-                        console.log(state);
-                    })
-                    .catch(function(err){
-
-                    });
-                priority3.shift();
-            }
-            else if(obj.getActiveCalls() < obj.maxCalls && priority4[0]){
-                priority4[0].startCall(obj.ari, obj.appName)
-                    .then(function(state){
-                        console.log(state);
-                    })
-                    .catch(function(err){
-
-                    });
-                priority4.shift();
-            }
-            else if(obj.getActiveCalls() < obj.maxCalls && priority5[0]){
-                priority5[0].startCall(obj.ari, obj.appName)
-                    .then(function(state){
-                        console.log(state);
-                    })
-                    .catch(function(err){
-
-                    });
-                priority5.shift();
-            }
-            if(!priority1[0]){
-                console.log('all empty');
-                clearInterval(intervalId);
-            }
-        }, 60);
+    var obj = this;
+    var priority1 = _.filter(obj.calls, function(element){return (element.priority == 1)});
+    var priority2 = _.filter(obj.calls, function(element){return (element.priority == 2)});
+    var priority3 = _.filter(obj.calls, function(element){return (element.priority == 3)});
+    var priority4 = _.filter(obj.calls, function(element){return (element.priority == 4)});
+    var priority5 = _.filter(obj.calls, function(element){return (element.priority == 5)});
+    var intervalId = setInterval(function(){
+        console.log(obj.getActiveCalls());
+        if(obj.getActiveCalls() < obj.maxCalls && priority1[0]){
+            priority1[0].startCall(obj.ari, obj.appName)
+                .then(function(state){
+                    console.log(state);
+                })
+                .catch(function(err){
+                });
+            priority1.shift();
+        }
+        else if(obj.getActiveCalls() < obj.maxCalls && priority2[0]){
+            priority2[0].startCall(obj.ari, obj.appName)
+                .then(function(state){
+                    console.log(state);
+                })
+                .catch(function(err){
+                });
+            priority2.shift();
+        }
+        else if(obj.getActiveCalls() < obj.maxCalls && priority3[0]){
+            priority3[0].startCall(obj.ari, obj.appName)
+                .then(function(state){
+                    console.log(state);
+                })
+                .catch(function(err){
+                });
+            priority3.shift();
+        }
+        else if(obj.getActiveCalls() < obj.maxCalls && priority4[0]){
+            priority4[0].startCall(obj.ari, obj.appName)
+                .then(function(state){
+                    console.log(state);
+                })
+                .catch(function(err){
+                });
+            priority4.shift();
+        }
+        else if(obj.getActiveCalls() < obj.maxCalls && priority5[0]){
+            priority5[0].startCall(obj.ari, obj.appName)
+                .then(function(state){
+                    console.log(state);
+                })
+                .catch(function(err){
+                });
+            priority5.shift();
+        }
+        if(!priority1[0]){
+            console.log('all empty');
+            clearInterval(intervalId);
+        }
+    }, 60);
 };
 
 module.exports = {
