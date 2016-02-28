22683556ff0dee0145074fb88884f033975ef67d
plomba97 <plomba97@gmail.com>
2016-02-28 16:42:54 +0200

    asd

--- routes/inform.js
+++ routes/inform.js
@@ -50,7 +50,6 @@ router.post('/', function(req, res, next) {
 //Route: /inform/callsData Method:GET - Renders groups
 router.get('/callsData', function(req, res, next) {
     var calls = callManager.createManager({ari:{}, maxCalls: 10, appName: 'testPeople'}).calls;
-    console.log({calls: calls});
     res.send({calls: calls});
 });
 
