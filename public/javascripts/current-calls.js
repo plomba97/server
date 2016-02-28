ac493a6e549ce159b94c202ccf5e0d3925bebfc0
plomba97 <plomba97@gmail.com>
2016-02-28 10:18:11 +0200

    changes

--- public/javascripts/current-calls.js
+++ public/javascripts/current-calls.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-    $.get('../../templates/current-calls.handlebars.html', function (data) {
-        var template=Handlebars.compile(data);
+    $.get('../../templates/current-calls.handlebars.html', function (hbtempl) {
+        var template=Handlebars.compile(hbtempl);
         //console.log(template({}));
         $.get('callsData', function(data){
-            console.log(data);
+            console.log(template(data));
         });
     }, 'html')
 });
