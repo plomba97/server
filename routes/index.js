ec30e60097556650fc6ebe465604b10935b63ddd
plomba97 <plomba97@gmail.com>
2016-03-04 23:11:46 +0200

    changes

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', {signedUser: req.user, title:'Моля влезте в профила си'});
+    res.render('index', {signedUser: req.user});
 });
 
 module.exports = router;
