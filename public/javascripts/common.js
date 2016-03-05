09d666d5b78ab0c00c3b3cff2e396a7e405a844c
plomba97 <plomba97@gmail.com>
2016-03-05 14:46:56 +0200

    changes

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -1,11 +1,30 @@
 $(document).ready(function(){
-    $('td.checkbox-container').on('click', function(event){
+    $('.table').on('click', function(event){
         //$(this).toggleClass('active');
+
         if(!(event.target.type === 'checkbox')){
-            var $checkBox = $(this).find('input:checkbox');
+            var $checkBox = $(event.target).find('input:checkbox');
             $checkBox.prop("checked", !$checkBox.prop("checked"));
         }
+        var id;
+        if($(event.target).hasClass('person-info')){
+
+            id = $(event.target).siblings('.checkbox-container').find('input:checkbox').attr('id');
+            $('#infoModal').modal('show');
+            $.get('/data/personInfo/' + id, function(data){
+                $('#infoModal').find('.modal-body').html(data);
+            });
+        }
+        else if($(event.target).hasClass('group-info')){
+            id = $(event.target).siblings('.checkbox-container').find('input:checkbox').attr('id');
+            $('#infoModal').modal('show');
+            console.log(id);
+            $.get('/data/groupInfo/' + id, function(data){
+                $('#infoModal').find('.modal-body').html(data);
+            });
+        }
     });
+
     $('.data-table').DataTable();
 
 });
--- routes/data.js
+++ routes/data.js
@@ -1,6 +1,7 @@
 var _ = require('underscore');
 var express = require('express');
 var Person = require('../data-models/person-model.js');
+var Group = require('../data-models/group-model.js');
 var router = express.Router();
 var callManager = require('../services/asterisk/calls-manager');
 
@@ -12,11 +13,19 @@ router.get('/calls', function(req, res, next) {
 });
 
 router.get('/personInfo/:id', function(req, res, next) {
-    var groupId = req.params.id;
-    Person.findOne({_id: groupId}).populate('groups').exec(function(err, data) {
+    var personId = req.params.id;
+    Person.findOne({_id: personId}).populate('groups').exec(function(err, data) {
         res.render('person-info', {person: data});
     });
 });
+
+router.get('/groupInfo/:id', function(req, res, next) {
+    var groupId = req.params.id;
+    Group.findOne({_id: groupId}).populate('people').exec(function(err, data) {
+        res.render('group-info', {group: data});
+    });
+});
+
 
 module.exports = router;
 
